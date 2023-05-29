bb1a49a7fe944401bb00f6429f36f0c2d568958e
G0vinda <frederickfreund@gmail.com>
2023-05-29 21:37:07 +0200

    Add Toon Shader, Switch to android build

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,14 +8,20 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private GameObject clickEffectPrefab;
     
     private Input _input;
     private Camera _cam;
     private NavMeshAgent _navMeshAgent;
+    private Animator _animator;
+    private bool _isWalking;
+    private int _walkForwardHash;
     
     private void Awake()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
+        _walkForwardHash = Animator.StringToHash("Walk Forward");
+        _animator = GetComponent<Animator>();
         _input = new Input();
         _cam = Camera.main;
     }
@@ -32,14 +38,29 @@ public class PlayerController : MonoBehaviour
         _input.Player.MovePress.performed -= ProcessMoveInput;
     }
 
+    private void Update()
+    {
+        if(!_isWalking)
+            return;
+
+        if (!_navMeshAgent.hasPath)
+        {
+            _isWalking = false;
+            _animator.SetBool(_walkForwardHash, false);
+        }
+    }
+
     private void ProcessMoveInput(InputAction.CallbackContext context)
     {
-        Debug.Log("Input detected");
+        
         var inputPosition = _input.Player.MovePosition.ReadValue<Vector2>();
         var ray = _cam.ScreenPointToRay(inputPosition);
-        if (Physics.Raycast(ray, out var hit, 100, groundLayer))
+        if (Physics.Raycast(ray, out var hit, 1000, groundLayer))
         {
             _navMeshAgent.SetDestination(hit.point);
+            Instantiate(clickEffectPrefab, hit.point, Quaternion.Euler(new Vector3(-90, 0 , 0)));
+            _isWalking = true;
+            _animator.SetBool(_walkForwardHash, true);
         }
     }
 }
