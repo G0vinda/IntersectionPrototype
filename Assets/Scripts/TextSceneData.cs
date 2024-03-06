1fa8a96077a8983f38842f591384209c8259fe5c
G0vinda <frederickfreund@gmail.com>
2024-03-06 15:40:36 +0100

    Add Text Areas to textScreens

--- Assets/Scripts/TextSceneData.cs
+++ Assets/Scripts/TextSceneData.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewTextSceneData", menuName = "SceneData/TextSceneData", order = 100)]
 public class TextSceneData : SceneData
 {
+    [TextArea]
     public string text;
     public TextSceneData page2;
 }
